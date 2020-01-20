d74a47ec7a9405080d17fc98cb4048a68341bbe4
Isaac Colls <colls_isaac@hotmail.com>
2020-01-20 16:42:18 -0300

    pipes movement

--- src/Physics.js
+++ src/Physics.js
@@ -1,4 +1,5 @@
 import Matter from 'matter-js';
+import Constants from './Constants';
 
 const Physics = (entities, { input, time }) => {
     let engine = entities.physics.engine;
@@ -7,6 +8,23 @@ const Physics = (entities, { input, time }) => {
     input.filter(t => t.name === "onClick").forEach(t => {
         Matter.Body.applyForce( bird, bird.position, { x: 0.00, y: -0.10 });
     });
+
+    for(let i=1; i<=4; i++) {
+        if (entities["pipe" + i].body.position.x <= -1 * (Constants.PIPE_WIDTH / 2)) {
+            Matter.Body.setPosition(entities["pipe" + i].body, { x: Constants.MAX_WIDTH * 2 - (Constants.PIPE_WIDTH / 2), y: entities["pipe" + i].body.position.y });
+        } else {
+            Matter.Body.translate(entities["pipe" + i].body, { x: -1, y: 0 });
+        }
+    }
+
+
+    // for(let i=1; i<=4; i++){
+    //     if (entities["pipe" + i].body.position.x <= -1 * (Constants.PIPE_WIDTH / 2)){
+    //         Matter.Body.setPosition( entities["pipe" + i].body, {x: Constants.MAX_WIDTH * 2 - (Constants.PIPE_WIDTH / 2), y: entities["pipe" + i].body.position.y});
+    //     } else {
+    //         Matter.Body.translate( entities["pipe" + i].body, {x: -1, y: 0});
+    //     }
+    // }
 
     Matter.Engine.update(engine, time.delta);
 
