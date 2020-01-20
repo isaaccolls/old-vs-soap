0f82f52d579cf76ae713a5bf9d6669b07c4e4be0
Isaac Colls <colls_isaac@hotmail.com>
2020-01-20 11:49:30 -0300

    just paint a purple box

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+// import logo from './logo.svg';
 import './App.css';
+import Constants from './Constants';
+import { GameEngine } from 'react-game-engine';
+import Matter from 'matter-js';
+import Bird from './Bird';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.gameEngine = null;
+    this.entities = this.setupWorld();
+  }
+
+  setupWorld = () => {
+    let engine = Matter.Engine.create({ enableSleeping: false });
+    let world = engine.world;
+
+    let bird = Matter.Bodies.rectangle(Constants.MAX_WIDTH / 4, Constants.MAX_HEIGHT / 2, 50, 50);
+    Matter.World.add(world, [bird]);
+
+    return {
+      pyshics: { engine: engine, world: world },
+      bird: { body: bird, size: [50, 50], color: 'purple', renderer: Bird },
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+          <p>IC</p>
+          <GameEngine
+            ref={(ref) => {this.gameEngine = ref; }}
+            style={styles.gameContainer}
+            entities={this.entities}
+          />
+      </div>
+    );
+  }
+
+}
+
+const styles = {
+  gameContainer: {
+    flex: 1,
+    width: 800,
+    height: 600,
+    backgroundColor: 'lightblue',
+  }
 }
 
 export default App;
---
+++ src/Constants.js
@@ -0,0 +1,10 @@
+const { innerWidth: width, innerHeight: height } = window;
+
+const Constants = {
+    MAX_WIDTH: width,
+    MAX_HEIGHT: height,
+    GAP_ZISE: 200,
+    PIPE_WIDTH: 100,
+}
+
+export default Constants;
