5e2ca1b736a6d3c73d33434969f3a9783554baaa
Isaac Colls <colls_isaac@hotmail.com>
2020-01-20 16:22:25 -0300

    generate pipes

--- src/App.js
+++ src/App.js
@@ -8,6 +8,23 @@ import Bird from './Bird';
 import Wall from './Wall';
 import Physics from './Physics';
 
+export const randomBetween = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+export const generatePipes = () => {
+  let topPipeHeight = randomBetween(100, (Constants.MAX_HEIGHT / 2) - 100);
+  let bottomPipe = Constants.MAX_HEIGHT - topPipeHeight - Constants.GAP_ZISE;
+
+  let sizes = [topPipeHeight, bottomPipe];
+  
+  if (Math.random() < 0.5) {
+    sizes = sizes.reverse();
+  }
+
+  return sizes;
+}
+
 class App extends Component {
 
   constructor(props) {
@@ -24,13 +41,27 @@ class App extends Component {
     let floor = Matter.Bodies.rectangle(Constants.MAX_WIDTH / 2, Constants.MAX_HEIGHT - 25, Constants.MAX_WIDTH, 50, { isStatic: true });
     let ceiling = Matter.Bodies.rectangle(Constants.MAX_WIDTH / 2, 25, Constants.MAX_WIDTH, 50, { isStatic: true });
 
-    Matter.World.add(world, [bird, floor, ceiling]);
+    let [pipe1Height, pipe2Height] = generatePipes();
+    let pipe1 = Matter.Bodies.rectangle(Constants.MAX_WIDTH - (Constants.PIPE_WIDTH / 2), pipe1Height / 2, Constants.PIPE_WIDTH, pipe1Height, { isStatic: true });
+    let pipe2 = Matter.Bodies.rectangle(Constants.MAX_WIDTH - (Constants.PIPE_WIDTH / 2), Constants.MAX_HEIGHT - (pipe2Height / 2), Constants.PIPE_WIDTH, pipe2Height, { isStatic: true });
+
+    let [pipe3Height, pipe4Height] = generatePipes();
+    let pipe3 = Matter.Bodies.rectangle(Constants.MAX_WIDTH * 2 - (Constants.PIPE_WIDTH / 2), pipe3Height / 2, Constants.PIPE_WIDTH, pipe3Height, { isStatic: true });
+    let pipe4 = Matter.Bodies.rectangle(Constants.MAX_WIDTH * 2 - (Constants.PIPE_WIDTH / 2), Constants.MAX_HEIGHT - (pipe4Height / 2), Constants.PIPE_WIDTH, pipe4Height, { isStatic: true });
+
+
+
+    Matter.World.add(world, [bird, floor, ceiling, pipe1, pipe2, pipe3, pipe4]);
 
     return {
       physics: { engine: engine, world: world },
       bird: { body: bird, size: [50, 50], color: 'purple', renderer: Bird },
       floor: { body: floor, size: [Constants.MAX_WIDTH, 50], color: 'green', renderer: Wall },
       ceiling: { body: ceiling, size: [Constants.MAX_WIDTH, 50], color: 'green', renderer: Wall},
+      pipe1: { body: pipe1, size: [Constants.PIPE_WIDTH, pipe1Height], color: 'yellow', renderer: Wall},
+      pipe2: { body: pipe2, size: [Constants.PIPE_WIDTH, pipe2Height], color: 'yellow', renderer: Wall},
+      pipe3: { body: pipe3, size: [Constants.PIPE_WIDTH, pipe3Height], color: 'yellow', renderer: Wall},
+      pipe4: { body: pipe4, size: [Constants.PIPE_WIDTH, pipe4Height], color: 'yellow', renderer: Wall},
     }
   }
 
