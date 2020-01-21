226070a456d07ba03655232064ca30d05bf09f7a
Isaac Colls <colls_isaac@hotmail.com>
2020-01-21 16:22:35 -0300

    bird flapping

--- src/Physics.js
+++ src/Physics.js
@@ -1,5 +1,7 @@
 import Matter from 'matter-js';
-import Constants from './Constants';
+
+let tick = 0;
+let pose = 1;
 
 const Physics = (entities, { input, time }) => {
     let engine = entities.physics.engine;
@@ -9,8 +11,16 @@ const Physics = (entities, { input, time }) => {
         Matter.Body.applyForce( bird, bird.position, { x: 0.00, y: -0.10 });
     });
 
-    
     Matter.Engine.update(engine, time.delta);
+
+    tick += 1;
+    if (tick % 5 === 0) {
+        pose = pose + 1;
+        if (pose > 3) {
+            pose = 1;
+        }
+        entities.bird.pose = pose;
+    }
 
     return entities;
 }
