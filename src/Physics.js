4ec796c209dd5f2377a48890ff3c28b5d5974b54
Isaac Colls <colls_isaac@hotmail.com>
2020-01-21 16:32:19 -0300

    move the floor

--- src/Physics.js
+++ src/Physics.js
@@ -1,4 +1,5 @@
 import Matter from 'matter-js';
+import Constants from './Constants';
 
 let tick = 0;
 let pose = 1;
@@ -12,6 +13,16 @@ const Physics = (entities, { input, time }) => {
     });
 
     Matter.Engine.update(engine, time.delta);
+
+    Object.keys(entities).forEach(key => {
+        if (key.indexOf("floor") === 0) {
+            if (entities[key].body.position.x <= -1 * (Constants.MAX_WIDTH / 2)) {
+                Matter.Body.setPosition(entities[key].body, {x: Constants.MAX_WIDTH + (Constants.MAX_WIDTH / 2), y: entities[key].body.position.y});
+            } else {
+                Matter.Body.translate(entities[key].body, {x: -2, y: 0});
+            }
+        }
+    });
 
     tick += 1;
     if (tick % 5 === 0) {
